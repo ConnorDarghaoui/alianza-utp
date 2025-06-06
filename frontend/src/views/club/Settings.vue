3b46b7b4227a61ac4069e43e82cfc4464382cee1
Lucas Boniche <luisboniche@ya.ru>
2025-06-05 23:42:03 -0500

    feat: extend club store with history and social links

--- frontend/src/views/club/Settings.vue
+++ frontend/src/views/club/Settings.vue
@@ -2,14 +2,14 @@
 import { onMounted } from 'vue'
 import { useRoute }   from 'vue-router'
 import { useClubStore } from '@/store/useClubStore'
+import { storeToRefs } from 'pinia'
 import LucideIcon from '@/components/ui/LucideIcon.vue'
 
 const clubStore = useClubStore()
 const route     = useRoute()
 const id        = Number(route.params.id)
 
-// extraemos con storeToRefs() si quieres
-// const { details, history, accessLog } = storeToRefs(clubStore)
+const { details, history, accessLog } = storeToRefs(clubStore)
 
 onMounted(() => {
   // solo a través del store
@@ -19,14 +19,14 @@ onMounted(() => {
 })
 
 function saveDetails() {
-  clubStore.updateDetails(id, clubStore.details)
+  clubStore.updateDetails(id, details.value)
 }
 
 function saveSocial() {
   clubStore.updateSocialLinks(id, {
-    facebook : clubStore.details.facebook,
-    instagram: clubStore.details.instagram,
-    twitter  : clubStore.details.twitter
+    facebook : details.value.facebook,
+    instagram: details.value.instagram,
+    twitter  : details.value.twitter
   })
 }
 </script>
@@ -36,19 +36,19 @@ function saveSocial() {
 
   <!-- Información del Club -->
   <div>
-    <input v-model="clubStore.details.name" … />
+    <input v-model="details.name" … />
     <!-- resto igual -->
     <button @click="saveDetails">Guardar cambios</button>
   </div>
 
   <!-- Redes Sociales -->
   <div>
-    <input v-model="clubStore.details.facebook" … />
+    <input v-model="details.facebook" … />
     <!-- resto igual -->
     <button @click="saveSocial">Guardar enlaces</button>
   </div>
 
   <!-- Historial y accesos -->
-  <div v-for="h in clubStore.history" :key="h.id">…</div>
-  <div v-for="r in clubStore.accessLog" :key="r.id">…</div>
+  <div v-for="h in history" :key="h.id">…</div>
+  <div v-for="r in accessLog" :key="r.id">…</div>
 </template>
